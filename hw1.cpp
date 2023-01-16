3e97095ecca356a5026c8db97bbd602ed8b1e878
profounddark <39137591+profounddark@users.noreply.github.com>
2023-01-16 10:26:08 -0800

    completed requirements for the assignment

--- hw1.cpp
+++ hw1.cpp
@@ -7,9 +7,17 @@ using std::cout, std::cin, std::endl;
 // Parameters:  the number, and the exponent - exp
 // Returns:     0 ix exp is negative, 1 if exp i 0; raise num to the exp
 //              power otherwise.
-
 int power(int num, int exp);
 
+// Function:    myLog
+// Purpose:     compute the nearest integer log (rounded down)
+// Parameters:  the base and the value to compute
+// Returns:     -1 if value is 0 or negative; the log base of value,
+//              rounded down to the nearest int
+int myLog(int base, int value);
+
+
+/*********************************************************************/
 
 /**
  * power performs an exponentiation operation, raising the base num
@@ -35,10 +43,35 @@ int power(int num, int exp) {
         }
     }
 
+    // return result
     return result;
 
 }
 
+/**
+ * myLog performs a basic integer log calculation, rounded down to the
+ * nearest integer
+ * @param base the base of the log calculation
+ * @param value the value to calculate for
+ * @return -1 if value was 0 or negative, otherwise the log calculation
+ * rounded down to the nearest integer
+ */
+int myLog(int base, int value) {
+
+    int result = 0;
+
+    // loop until base ^ result > value
+    while (power(base, result) <= value) {
+        result++;
+    }
+
+    // the while loop went one too far, so subtract one
+    return result - 1;
+}
+
+/**
+ * testPower prompts the user for data in order to test the power function.
+ */
 void testPower() {
     // variables for receiving input from user
     int testBase = 0;
@@ -59,10 +92,40 @@ void testPower() {
     cout << power(testBase, testPower) << endl;
 }
 
+/**
+ * testLog prompts the user for data in order to test the myLog function
+ */
+void testLog() {
+    // variables for receiving input from user
+    int testBase = 0;
+    int testNumber = 0;
+
+    // prompt user for base and input value
+    cout << "Enter a base: ";
+    cin >> testBase;
+    // prompt user for number and input value
+    cout << "Enter a number: ";
+    cin >> testNumber;
+
+    // for whitespacing
+    cout << endl;
+
+    // display output
+    cout << "Log of " << testNumber << " in base " << testBase << " is ";
+    cout << myLog(testBase, testNumber) << endl;
+}
+
 int main() {
 
     // run function to test power
     testPower();
+
+    // for whitespacing
+    cout << endl;
+
+    // run funciton to test myLog
+    testLog();
+
 
     return 0;
 }
